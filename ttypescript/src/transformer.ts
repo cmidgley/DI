27226dbf4130048f6aa69be19a0b7e27a5976a79
Chris <chris@koose.com>
2022-01-09 20:35:30 +0000

    Removed commented out debug code

--- ttypescript/src/transformer.ts
+++ ttypescript/src/transformer.ts
@@ -1,5 +1,4 @@
 import type { Program } from "typescript";
 import { di } from "@wessberg/di-compiler";
 const transformer = (program: Program) => di({ program });
-// console.log("In transformer");
 export default transformer;
